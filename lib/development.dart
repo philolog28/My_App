6f9c7d5899d5525aca6cb2fa7f47cb43e537e9f9
philolog28 <ivan.vanyushonkov@yandex.ru>
2022-03-23 19:59:32 +0300

    а

--- lib/development.dart
+++ lib/development.dart
@@ -3,8 +3,5 @@ import 'package:commerce_app/env.dart';
 Future<void> main() async => Development().init();
 
 class Development extends Env {
-  final String baseUrl = 'https://novex-tl29457159-app.dev.creonit.ru/';
-  final bool showAlice = true;
-  final bool writeLogs = true;
-  final String siteUrl = '';
+
 }
